be4b537eb3cb5c9f6f1a85f90bef565ddc156081
dominod <dbdomino@naver.com>
2023-10-31 22:34:08 +0900

    start

--- guiSample23.py
+++ guiSample23.py
@@ -23,26 +23,28 @@ def value_checkbox():
     #print("체스박스선택 = ",checkbox01value) # PY_VAR0
     print("오늘 하루 그만 보기 = ", checkbox01value.get()) # 0 비선택, 1 선택
 
-button01 = Button(root, text="checkbox01value", command=value_checkbox) #값 삭제
-button01.pack()
 
-# 라디오 버튼, 여러 선택지 중 하나만 선택가능 
-radio_var = IntVar() # 여기에 라디오버튼 값 저장
-button_burger01 = Radiobutton(root, text="데리버거", value=1, variable=radio_var)
-button_burger02 = Radiobutton(root, text="치킨버거", value=2, variable=radio_var)
-button_burger03 = Radiobutton(root, text="새우버거", value=3, variable=radio_var)
-button_burger04 = Radiobutton(root, text="불고기버거", value=5, variable=radio_var)
-button_burger05 = Radiobutton(root, text="라이스버거", value=7, variable=radio_var)
-button_burger01.pack()
-button_burger02.pack()
-button_burger03.pack()
-button_burger04.pack()
-button_burger05.pack()
+# 라디오 버튼, 값 텍스트로 저장
+radio_var02 = StringVar() # 여기에 라디오버튼 값 저장
+button_burger06 = Radiobutton(root, text="데리버거", value="데리버거", variable=radio_var02)
+button_burger06.select()
+button_burger07 = Radiobutton(root, text="치킨버거", value="치킨버거", variable=radio_var02)
+button_burger08 = Radiobutton(root, text="새우버거", value="새우버거", variable=radio_var02)
+button_burger09 = Radiobutton(root, text="불고기버거", value="불고기버거", variable=radio_var02)
+button_burger10 = Radiobutton(root, text="라이스버거", value="라이스버거", variable=radio_var02)
+button_burger06.pack()
+button_burger07.pack()
+button_burger08.pack()
+button_burger09.pack()
+button_burger10.pack()
 
-def print_radio():
+def print_radio02():
     #print("라디오 버튼 선택 = ",radio_var) # PY_VAR0
-    print("주문한 값 = ", radio_var.get()) # 0 비선택, 1 선택
-button02 = Button(root, text="주문하기", command=print_radio) #값 삭제
-button02.pack()
+    print("주문한 값 = ", radio_var02.get()) # 0 비선택, 1 선택
+button03 = Button(root, text="한글 주문하기", command=print_radio02) #값 삭제
+button03.pack()
+
+
+
 
 root.mainloop() # 루트에 메인 루프 선언, 루프가 작동되면서 Tk()로 선언된 gui 화면이 동작시작
